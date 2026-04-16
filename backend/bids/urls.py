6db94849b4940a5c846c323de0380e97b339d86d
rstmschf <kshevketsheva@gmail.com>
2026-04-16 16:18:48 +0200

    refactor: removed unused files + dead code

--- backend/bids/urls.py
+++ backend/bids/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .consumers import AuctionConsumer
 
-urlpatterns = []
 
 websocket_urlpatterns = [
     path("ws/auctions/<int:auction_id>/", AuctionConsumer.as_asgi()),
